65b9d141da5dcc3aef1fe1305988943b4223ec1d
toshimizuguchi <toshi0123.mizuguchi@gmail.com>
2025-04-11 12:34:56 -0300

    estilização DPSins

--- src/Components/DpsInstall.jsx
+++ src/Components/DpsInstall.jsx
@@ -1,17 +1,17 @@
-
+import "./styles/DpsInstall.css";
 
 const DpsInstall = () => {
   return (
-    <div>
+    <div class="container">
       <h1>Obrigado por baixar nosso sistema!</h1>
       <p>Agora, siga os passos para se cadastrar.</p>
       {/* Aqui você pode adicionar mais informações ou links para o cadastro */}
       <br></br>
       <p>Clique no botao para se cadastrar</p>
-      <div className="btn-cadastro">
+      
         <button type="button">Cadastre-se</button>
         
-    </div>
+   
     </div>
     
   );
